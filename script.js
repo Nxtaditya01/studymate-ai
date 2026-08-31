a9002ed51246a608508ea419484254d931c4a876
Nxtaditya01 <adityarajff98@gmail.com>
2026-08-31 10:36:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-console.log("JS loaded");
 function askAI(){
 
-    let question = document.querySelector("input").value;
-    let answerBox = document.querySelector(".ai");
+let question = document.querySelector("input").value.toLowerCase();
+let answerBox = document.querySelector(".ai");
 
-    if(question.trim() === ""){
-        answerBox.innerHTML = "❌ Pehle question likho";
-    }
-    else{
-        answerBox.innerHTML = 
-        "🤖 AI: Tumhara question hai - " + question + 
-        "<br><br>📚 Main iska answer prepare kar raha hu.";
-    }
+if(question.trim() === ""){
+    answerBox.innerHTML = "❌ Pehle question likho";
+}
+
+else if(question.includes("photosynthesis")){
+    answerBox.innerHTML = 
+    "🌱 Photosynthesis ek process hai jisme plants sunlight, water aur carbon dioxide ki help se apna food banate hain.";
+}
+
+else if(question.includes("gravity")){
+    answerBox.innerHTML =
+    "🌍 Gravity ek force hai jo objects ko Earth ki taraf attract karta hai.";
+}
+
+else if(question.includes("hello") || question.includes("hi")){
+    answerBox.innerHTML =
+    "👋 Hello! Main StudyMate AI hu. Tum apna study doubt puch sakte ho.";
+}
+
+else{
+    answerBox.innerHTML =
+    "🤖 Main is topic ko samajhne ki koshish kar raha hu. Thoda aur detail me question pucho.";
+}
 
 }
