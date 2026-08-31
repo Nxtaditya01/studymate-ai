259180891a105348778fcdbbf956c4404e7c2de0
Nxtaditya01 <adityarajff98@gmail.com>
2026-08-31 10:27:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert("StudyMate AI Ready!");
+function askAI(){
+
+let question = document.querySelector("input").value;
+
+let answerBox = document.querySelector(".ai");
+
+
+if(question==""){
+    answerBox.innerHTML="Please enter your question 🤔";
+}
+else{
+
+answerBox.innerHTML=
+"🤖 AI: "+question+" ka answer prepare ho raha hai...<br><br>"+
+"📚 Pehle concept samjho, phir examples practice karo.";
+
+}
+
+}
